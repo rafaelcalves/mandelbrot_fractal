adca65974e5b396e90762531fef466f1ff1d961f
Rafael Correa <rafael.correa@sap.com>
2021-05-30 19:54:06 -0300

    Removed unecessary code from main

--- main.cpp
+++ main.cpp
@@ -7,23 +7,12 @@
 
 #include "x11_config.h"
 
-Display *dis;
-int screen;
-Window win;
-GC gc;
-unsigned long black, white, red, blue;
-
 int main() {
     X11Config x11;
-    XEvent event;
-    KeySym key;
-    char text[255];
     while (1)
     {
         x11.handleEvents();
     }
-
-    return 0;
 }
 
 
